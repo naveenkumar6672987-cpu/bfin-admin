bdbaaec4c4e4b8927c49aaa3ea7e6bdeddd261b5
naveenkumar6672987-cpu <naveenkumar6672987@gmail.com>
2026-02-17 14:08:23 +0530

    Update AdminLayout.js

--- src/Admin/AdminLayout.js
+++ src/Admin/AdminLayout.js
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import {Outlet } from "react-router-dom";
-import { AdminAuthContext } from "../context/AdminAuthContext";
+
 
 
 
